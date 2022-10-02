12c72bf3c34aca8be5795d41725524815fcd2af2
esponges <esponges@gmail.com>
2022-10-02 16:15:50 -0500

    add items to cart

--- src/pages/product/[productId]/index.tsx
+++ src/pages/product/[productId]/index.tsx
@@ -1,25 +1,52 @@
-import { useRouter } from "next/router";
-import { trpc } from "../../../utils/trpc";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useCartStore } from '../../../store/cart';
+import { trpc } from '../../../utils/trpc';
 
 const ProductDetails = () => {
   const router = useRouter();
-  const id = typeof router.query.productId === "string" ? router.query.productId : '';
+  const id = typeof router.query.productId === 'string' ? router.query.productId : '';
 
   const { data: product, isLoading } = trpc.useQuery(['product.getById', { id }]);
+
+  const { addToCart, items } = useCartStore((state) => state);
+  const itemCartCount = items[id]?.quantity || 0;
+  const [count, setCount] = useState(0);
+
+  const handleAddToCart = () => {
+    if (product) {
+      addToCart(product, count);
+    }
+  };
 
   if (isLoading) {
     return <p>Loading product {id}...</p>;
   }
-  
+
   return (
-    <div>
+    <div className='px-10 py-5'>
       <h1>Product Details</h1>
       <p>Product ID: {product?.id}</p>
       <p>Product Name: {product?.name}</p>
       <p>Product Description: {product?.description}</p>
-      <button onClick={() => router.push('/')}>Go back</button>
+      <p>Product Price: {product?.price}</p>
+      <p>In the cart {itemCartCount}</p>
+      <div className='mt-5'>
+        <p>
+          Quantity: {count}{' '}
+          <span>
+            <button onClick={() => setCount(count + 1)}>Add +</button>
+          </span>
+        </p>
+        <button className="mt-1" onClick={handleAddToCart}>
+          Add to cart
+        </button>
+      </div>
+      <button className="mt-5" onClick={() => router.push('/')}>
+        Go back
+      </button>
     </div>
   );
-}
+};
 
 export default ProductDetails;
