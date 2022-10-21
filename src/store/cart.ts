7b5c90542a78e5264f0dbf1fbfd02f0531a2c8b8
esponges <esponges@gmail.com>
2022-10-20 19:27:41 -0500

    persist cart between sessions

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,24 +1,35 @@
-import { Product } from "@prisma/client";
-import create from "zustand";
+import { Product } from '@prisma/client';
+import create from 'zustand';
 
 type Item = Product & { quantity: number };
-
+type CartItems = { [key: string]: Item };
 interface CartState {
-  items: { [key: string]: Item };
+  items: CartItems;
   addToCart: (product: Product, quantity: number) => void;
 }
 
+const storageItems: CartItems = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('cart') || '{}') : {};
+
 export const useCartStore = create<CartState>((set) => ({
-  items: {},
+  items: storageItems,
   addToCart: (product, quantity) => {
     set((state) => {
       const item: Item = { ...product, quantity };
-      return {
-        items: { ...state.items, [product.id]: {
-          ...item,
-          quantity: (state.items[product.id]?.quantity || 0) + quantity,
-        } },
+
+      const updatedCart = {
+        items: {
+          ...state.items,
+          [product.id]: {
+            ...item,
+            quantity: (state.items[product.id]?.quantity || 0) + quantity,
+          },
+        },
       };
+
+      // persist cart to local storage
+      localStorage.setItem('cart', JSON.stringify(updatedCart.items));
+
+      return updatedCart;
     });
   },
 }));
