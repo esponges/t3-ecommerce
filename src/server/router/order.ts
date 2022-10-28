7afaf39b683fd072fb7439c3a0215782006f48c3
esponges <esponges@gmail.com>
2022-10-28 16:42:16 -0500

    fix sending array instead of object

--- src/server/router/order.ts
+++ src/server/router/order.ts
@@ -1,74 +1,77 @@
 import { z } from 'zod';
 import { createRouter } from './context';
 
-export const orderRouter = createRouter().mutation('create', {
-  input: z.object({
-    userId: z.string(),
-    orderItems: z.array(
-      z.object({
-        productId: z.string(),
-        quantity: z.number(),
-      })
-    ),
-    orderDetail: z.array(
-      z.object({
+export const orderRouter = createRouter()
+  .mutation('create', {
+    input: z.object({
+      userId: z.string(),
+      orderItems: z.array(
+        z.object({
+          productId: z.string(),
+          quantity: z.number(),
+        })
+      ),
+      orderDetail: z.object({
         address: z.string(),
         city: z.string(),
         country: z.string(),
         postalCode: z.string(),
         phone: z.string(),
-      })
-    ),
-  }),
-  resolve: async ({ ctx, input }) => {
-    try {
-      const order = await ctx.prisma.order.create({
-        data: {
-          userId: input.userId,
-          orderItems: {
-            create: input.orderItems,
+      }),
+    }),
+    resolve: async ({ ctx, input }) => {
+      try {
+        const order = await ctx.prisma.order.create({
+          data: {
+            userId: input.userId,
+            orderItems: {
+              create: input.orderItems,
+            },
+            orderDetail: {
+              create: input.orderDetail,
+            },
           },
-          orderDetail: {
-            create: input.orderDetail,
+        });
+        return order;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  })
+  .query('getByUserId', {
+    input: z.object({
+      userId: z.string(),
+      latest: z.boolean().optional(),
+    }),
+    resolve: async ({ ctx, input }) => {
+      try {
+        const order = await ctx.prisma.order.findMany({
+          // get the latest order
+          where: {
+            userId: input.userId,
           },
-        },
-      });
-      return order;
-    } catch (error) {
-      console.log(error);
-    }
-  },
-}).query('getByUserId', {
-  input: z.object({
-    userId: z.string(),
-    latest: z.boolean().optional(),
-  }),
-  resolve: async ({ ctx, input }) => {
-    try {
-      const order = await ctx.prisma.order.findMany({
-      // get the latest order
-        where: {
-          userId: input.userId,
-        },
-        ...(input.latest ? { orderBy: {
-          id: 'desc',
-        },
-        take: 1 } : {}),
-      });
-      return order;
-    }
-    catch (error) {
-      console.log(error);
-    }
-  },
-}).query('getAll', {
-  resolve: async ({ ctx }) => {
-    try {
-      const order = await ctx.prisma.order.findMany();
-      return order;
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-});
+          ...(input.latest
+            ? {
+                orderBy: {
+                  id: 'desc',
+                },
+                take: 1,
+              }
+            : {}),
+        });
+        return order;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  })
+  .query('getAll', {
+    resolve: async ({ ctx }) => {
+      try {
+        const order = await ctx.prisma.order.findMany();
+        return order;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
