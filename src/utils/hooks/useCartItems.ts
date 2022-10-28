abc6ccff5c5c7b0c1c2e34a008112168f9c4984f
esponges <esponges@gmail.com>
2022-10-27 20:18:24 -0500

    fix total sum

--- src/utils/hooks/useCartItems.ts
+++ src/utils/hooks/useCartItems.ts
@@ -4,7 +4,7 @@ import { useCartStore, CartItems } from '../../store/cart';
 export const useCartItems = () => {
   const { items } = useCartStore((state) => state);
   const [cartItems, setCartItems] = useState<CartItems>({});
-  const cartTotal = Object.values(cartItems).reduce((acc, item) => acc + item.quantity, 0);
+  const cartTotal = Object.values(cartItems).reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   // get rid of hydration problems
   // since we set them from the localStorage in the store
