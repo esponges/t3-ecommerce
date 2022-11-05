e3a763a4ffb4317288b5254d9d21dfc1340f8b15
esponges <esponges@gmail.com>
2022-11-05 17:36:53 -0600

    add login/logout in header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,6 +1,10 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { Button } from "../atoms/button";
 
 export const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex justify-between items-center py-5 px-10">
       <div className="flex items-center">
@@ -10,8 +14,20 @@ export const Header = () => {
       </div>
       <div className="flex items-center">
         <Link href="/cart">
-          <a className="font-bold text-2xl">Cart</a>
+          <Button variant='link' extraClassName="mr-2">
+            Cart
+          </Button>
         </Link>
+        {/* nextauth login */}
+        {!session ? (
+        <Button variant="primary" onClick={() => signIn('discord')}>
+          Login
+        </Button>
+        ) : (
+        <Button variant="primary" onClick={() => signOut()}>
+          Logout
+        </Button>
+        )}
       </div>
     </div>
   );
