f034eac7fff868824e5eebf99c9e56697e9b81af
esponges <esponges@gmail.com>
2022-11-05 17:36:36 -0600

    button atom

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,48 @@
+import { forwardRef } from 'react';
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+  variant: 'primary' | 'secondary' | 'link';
+  extraClassName?: string;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  const { onClick, children, variant, extraClassName } = props;
+
+  if (variant === 'primary') {
+    return (
+      <button
+        onClick={onClick}
+        ref={ref}
+        className={`rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 ${extraClassName}`}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  if (variant === 'link') {
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        className={`rounded bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100 ${extraClassName}`}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      ref={ref}
+      className={`rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100 ${extraClassName}`}
+    >
+      {children}
+    </button>
+  );
+});
+
+Button.displayName = 'Button';
