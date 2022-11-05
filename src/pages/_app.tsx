a38583dfc07a7a77fb8865b89f6937d90779ac8e
esponges <esponges@gmail.com>
2022-11-05 16:17:17 -0600

    add layout wrapper to enable per page layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,34 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import type { Session } from 'next-auth';
-import type { AppType } from 'next/app';
+import type { NextPage } from 'next';
+import type { AppType, AppProps } from 'next/app';
 import { trpc } from '../utils/trpc';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, ReactElement } from 'react';
 
-const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+  pageProps: {
+    session: Session | null;
+  }
+};
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout || ((page) => page);
+  const layout = getLayout(<Component {...pageProps} />);
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {layout}
       <ReactQueryDevtools />
     </SessionProvider>
   );
