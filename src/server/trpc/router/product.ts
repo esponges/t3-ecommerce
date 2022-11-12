5b218e72cefd5e528d85d16b532184d76416b157
esponges <esponges@gmail.com>
2022-11-12 14:26:22 -0600

    get batch for infinite query search

--- src/server/trpc/router/product.ts
+++ src/server/trpc/router/product.ts
@@ -5,13 +5,45 @@ export const productRouter = t.router({
   getAll: t.procedure.query(({ ctx }) => {
     return ctx.prisma.product.findMany();
   }),
-  getById: t.procedure
-    .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.product.findUnique({
+  getById: t.procedure.input(z.object({ id: z.string() })).query(({ ctx, input }) => {
+    return ctx.prisma.product.findUnique({
+      where: {
+        id: input.id,
+      },
+    });
+  }),
+  // get an array of products
+  // for infinite scroll
+  getBatch: t.procedure
+    .input(
+      z.object({
+        limit: z.number(),
+        cursor: z.string().nullish(),
+        skip: z.number().optional(),
+        categoryId: z.number().optional(),
+      })
+    )
+    .query(async({ ctx, input }) => {
+      const { limit, skip, categoryId, cursor } = input;
+      const items = await ctx.prisma.product.findMany({
+        take: limit + 1,
+        skip: skip,
+        cursor: cursor ? { id: cursor } : undefined,
+        orderBy: {
+          id: 'asc',
+        },
         where: {
-          id: input.id,
+          categoryId: categoryId ? categoryId : undefined,
         },
       });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (items.length > limit) {
+        const nextItem = items.pop();
+        nextCursor = nextItem?.id;
+      }
+      return {
+        items,
+        nextCursor,
+      };
     }),
 });
