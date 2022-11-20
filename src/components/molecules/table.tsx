20fe64a451d016f62fef2696865d2387dc9fff52
esponges <esponges@gmail.com>
2022-11-20 09:23:38 -0600

    basic styling

--- src/components/molecules/table.tsx
+++ src/components/molecules/table.tsx
@@ -1,4 +1,4 @@
-import { getCoreRowModel, useReactTable, flexRender } from '@tanstack/react-table';
+import { getCoreRowModel, useReactTable, flexRender, getFilteredRowModel, getPaginationRowModel } from '@tanstack/react-table';
 import type { ColumnDef } from '@tanstack/react-table';
 
 interface ReactTableProps<T extends object> {
@@ -11,32 +11,43 @@ export const Table = <T extends object>({ data, columns }: ReactTableProps<T>) =
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    // getFilteredRowModel: getFilteredRowModel(),
+    // getPaginationRowModel: getPaginationRowModel(),
+    debugTable: true,
   });
 
   return (
-    <div className="p-2">
-      <table className='border'>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-4 sm:px-6 lg:px-8">
+          <div className="overflow-hidden p-2">
+            <table className="min-w-full text-center">
+              <thead className="border-b bg-gray-50">
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <th key={header.id} className="px-6 py-4 text-sm font-medium text-gray-900">
+                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody>
+                {table.getRowModel().rows.map((row) => (
+                  <tr key={row.id} className='border-b" bg-white'>
+                    {row.getVisibleCells().map((cell) => (
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900" key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
