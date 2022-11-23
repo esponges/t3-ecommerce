eb2723ae06499b1f321d6781dd9e0218c1fb1e93
esponges <esponges@gmail.com>
2022-11-22 18:05:01 -0600

    center next last carousel arrows

--- src/components/organisms/productCarousel.tsx
+++ src/components/organisms/productCarousel.tsx
@@ -51,9 +51,13 @@ export const ProductCarousel = ({ category, extraClassName }: Props) => {
   const nextCursor = data?.pages[page]?.nextCursor;
 
   return (
-    <div className={`relative mt-6 flex flex-col items-center justify-center md:px-12 ${className.wrapper} ${extraClassName ?? ''}`}>
+    <div
+      className={`relative mt-6 flex flex-col items-center justify-center md:px-12 ${className.wrapper} ${
+        extraClassName ?? ''
+      }`}
+    >
       <h2 className="text-2xl font-bold text-gray-700">{category?.name ?? 'Products'}</h2>
-      <div className="mt-4 flex flex-row gap-4">
+      <div className="relative flex mt-2">
         {toShow?.map((product) => (
           <ProductCard
             key={product.id}
@@ -65,29 +69,29 @@ export const ProductCarousel = ({ category, extraClassName }: Props) => {
             onClick={() => handleCardClick(product.id)}
           />
         ))}
+        {nextCursor && (
+          <button
+            className="carousel-control-next absolute top-0 bottom-0 right-0 
+                flex items-center justify-center border-0 p-0 text-center 
+                hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+            type="button"
+            onClick={handleFetchNextPage}
+          >
+            <Image src="/arrow_forward.svg" width={50} height={50} layout="fixed" alt="next" />
+          </button>
+        )}
+        {page > 0 && (
+          <button
+            className="carousel-control-prev absolute top-0 bottom-0 left-0 
+                flex items-center justify-center border-0 p-0 text-center 
+                hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+            type="button"
+            onClick={handleFetchPreviousPage}
+          >
+            <Image src="/arrow_back.svg" width={50} height={50} layout="fixed" alt="next" />
+          </button>
+        )}
       </div>
-      {nextCursor && (
-        <button
-          className="carousel-control-next absolute top-0 bottom-0 right-0 
-              flex items-center justify-center border-0 p-0 text-center 
-              hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
-          type="button"
-          onClick={handleFetchNextPage}
-        >
-          <Image src="/arrow_forward.svg" width={50} height={50} layout="fixed" alt="next" />
-        </button>
-      )}
-      {page > 0 && (
-        <button
-          className="carousel-control-prev absolute top-0 bottom-0 left-0 
-              flex items-center justify-center border-0 p-0 text-center 
-              hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
-          type="button"
-          onClick={handleFetchPreviousPage}
-        >
-          <Image src="/arrow_back.svg" width={50} height={50} layout="fixed" alt="next" />
-        </button>
-      )}
     </div>
   );
 };
