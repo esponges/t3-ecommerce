fd83985f06771f20584b649a6d99e7f110e0801f
esponges <esponges@gmail.com>
2022-12-08 17:15:26 -0600

    navbar with semantic

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,37 +1,54 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { useState } from 'react';
+import { Visibility, Segment, Menu, Container, Button } from 'semantic-ui-react';
 
-import { Button } from '../atoms/button';
+// import { Button } from '../atoms/button';
 
 export const Header = () => {
   const { data: session } = useSession();
 
+  // might be usefull in the future
+  // const handleHideFixedMenu = () => setFixed(false);
+  // const handleShowFixedMenu = () => setFixed(true);
+
   return (
-    <div className="flex justify-between py-5 px-10
-      fixed top-0 left-0 right-0 z-10">
-      <div className="flex items-center">
-        <Link href="/">
-          <a className="text-2xl font-bold">Store</a>
-        </Link>
-        <Link href="/cart">
-          <a className="ml-4 text-2xl font-bold">Cart</a>
-        </Link>
-      </div>
-      <div className="flex items-center">
-        {/* nextauth login */}
-        {!session ? (
-          <Button variant="primary" onClick={() => signIn('discord')}>
-            Login
-          </Button>
-        ) : (
-          <>
-            <p className="mr-4">Hello —{session.user?.name}—</p>
-            <Button variant="primary" onClick={() => signOut()}>
-              Logout
-            </Button>
-          </>
-        )}
-      </div>
-    </div>
+    <Visibility 
+      once={false} 
+      // onBottomPassed={handleHideFixedMenu}
+      // onBottomPassedReverse={handleShowFixedMenu}
+      >
+      <Segment textAlign="center" vertical>
+        <Menu fixed={'top'} inverted={false} pointing secondary size="large">
+          <Container>
+            <Menu.Item as="a" active>
+              <Link href="/">
+                <a className="text-2xl font-bold">Store</a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Link href="/cart">
+                <a className="ml-4 text-2xl font-bold">Cart</a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item position="right">
+              {/* nextauth login */}
+              {!session ? (
+                <Button variant="primary" onClick={() => signIn('discord')}>
+                  Login
+                </Button>
+              ) : (
+                <>
+                  <p className="mr-4">Hello —{session.user?.name}—</p>
+                  <Button variant="primary" onClick={() => signOut()}>
+                    Logout
+                  </Button>
+                </>
+              )}
+            </Menu.Item>
+          </Container>
+        </Menu>
+      </Segment>
+    </Visibility>
   );
 };
