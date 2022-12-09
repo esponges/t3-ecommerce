11f74effcd28468573872977e220133d78c02202
esponges <esponges@gmail.com>
2022-12-09 17:19:20 -0600

    fix description h

--- src/components/molecules/productCard.tsx
+++ src/components/molecules/productCard.tsx
@@ -6,7 +6,6 @@ import type { Product } from '@prisma/client';
 
 import { Button } from '../atoms/button';
 import { Counter } from './counter';
-import { Header } from '../atoms/header';
 
 type Props = Partial<Product> & {
   onClick?: () => void;
@@ -53,6 +52,10 @@ export const ProductCard = ({
   };
 
   const handleAddToCart = () => {
+    if (onClick) {
+      onClick();
+    }
+
     if (onAddToCart) {
       setIsAddingToCart(true);
       onAddToCart(quantity);
@@ -65,15 +68,15 @@ export const ProductCard = ({
   };
 
   return (
-    <div className="card">
+    <div className="card m-2">
       <Card>
-        <Image src={image ?? '/empty-bottle.png'} wrapped ui={false} />
+        <Image src={image ?? '/empty-bottle.png'} alt='product' wrapped ui={false} />
         <Card.Content>
           <Card.Header>{name}</Card.Header>
           <Card.Meta>
             <span className="date">{price} MXN</span>
           </Card.Meta>
-          <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+          <Card.Description className='h-20'>{description}</Card.Description>
         </Card.Content>
         <Card.Content extra>
           <div className="text-center justify-center flex">
