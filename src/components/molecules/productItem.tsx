e91ea7971c80d9ebb885d102ac8ab4a57cc6fb2c
esponges <esponges@gmail.com>
2022-12-10 10:38:55 -0600

    product item view for details

---
+++ src/components/molecules/productItem.tsx
@@ -0,0 +1,51 @@
+import type { Product } from '@prisma/client';
+import { useState } from 'react';
+import { Button, Icon, Item, Label } from 'semantic-ui-react';
+import { useProduct } from '../../lib/hooks/useProduct';
+
+type Props = Partial<Product> & {
+  onClick?: () => void;
+  onAddToCart?: (qty: number) => void;
+};
+
+export const ProductItem = ({
+  name,
+  price,
+  description,
+  image,
+  id,
+  onClick,
+  onAddToCart,
+}: Props) => {
+  const {
+    quantity,
+    isAddingToCart,
+    handleChangeProductQty,
+    handleQtyInputChange,
+    handleAddToCart,
+  } = useProduct({});
+
+  return (
+    <Item>
+      <Item.Image src={image} alt='product' />
+      <Item.Content>
+        <Item.Header as='a'>{name}</Item.Header>
+        <Item.Meta>
+          <span className='cinema'>{price} MXN</span>
+        </Item.Meta>
+        <Item.Description>{description}</Item.Description>
+        <Item.Extra>
+          <Label>Category here</Label>
+          <Label icon='globe' content='Additional Languages' />
+          <Button.Group floated='right'>
+            <Button primary>
+              Buy tickets
+              {/* <Icon name='right chevron' /> */}
+            </Button>
+          </Button.Group>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+};
+
