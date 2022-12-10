e4df69048bb0d4ec34804ec83550500583b2361c
esponges <esponges@gmail.com>
2022-12-10 10:45:42 -0600

    ignore rules in context

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/require-await */
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
