76f8e5ca0e5bca7e450c72ebc61cee872297bb2c
esponges <esponges@gmail.com>
2022-12-13 16:52:25 -0600

    prepare middleware to handle admin dashboard routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,17 @@ import { NextResponse } from "next/server"
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(_req) {
+  function middleware(req) {
+    // check if the route is any of the admin dashboard routes
+    const nextUrl = req.nextUrl.pathname;
+    if (nextUrl.startsWith("/d") && !nextUrl.startsWith('/d/login') && !nextUrl.startsWith('/checkout')) {
+
+      // check for the admin user token
+      if (!req.cookies.get('store-admin-token')) {
+        return NextResponse.redirect(`${req.nextUrl.origin}/d/login`);
+      }
+    }
+    
     // let user proceed since they are authenticated
     return NextResponse.next();
   },
@@ -12,6 +22,7 @@ export default withAuth(
       authorized: ({ req, token: _token }) => {
         // token always returning null
         // temporary workaround is to use req.cookies
+        console.log('checking if authorized', req.cookies.get('next-auth.session-token'));
 
         // if truthy, user is authenticated - can proceed
         return !!req.cookies.get('next-auth.session-token');
@@ -20,4 +31,4 @@ export default withAuth(
   }
 )
 
-export const config = { matcher: ["/checkout/:path*"] }
+export const config = { matcher: ["/checkout/:path*", "/d/:path*"] }
