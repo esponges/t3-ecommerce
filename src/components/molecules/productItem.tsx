f257f554e8ae4add8cde68a532661a0dad3be70f
esponges <esponges@gmail.com>
2022-12-12 19:52:08 -0600

    add to cart handlers

--- src/components/molecules/productItem.tsx
+++ src/components/molecules/productItem.tsx
@@ -1,53 +1,45 @@
 import type { Product } from '@prisma/client';
-import { useState } from 'react';
-import { Button, Icon, Item, Label } from 'semantic-ui-react';
+import { Item, Label } from 'semantic-ui-react';
 import { useProduct } from '../../lib/hooks/useProduct';
+
+import { Button } from '../atoms/button';
+import { Counter } from './counter';
 
 type Props = Partial<Product> & {
   onClick?: () => void;
   onAddToCart?: (qty: number) => void;
 };
 
-export const ProductItem = ({
-  name,
-  price,
-  description,
-  image,
-  id,
-  onClick,
-  onAddToCart,
-}: Props) => {
-  const {
-    quantity,
-    isAddingToCart,
-    handleChangeProductQty,
-    handleQtyInputChange,
-    handleAddToCart,
-  } = useProduct({});
+export const ProductItem = ({ name, price, description, image, onAddToCart }: Props) => {
+  const { quantity, isAddingToCart, handleChangeProductQty, handleQtyInputChange, handleAddToCart } = useProduct({ onAddToCart });
 
   return (
-    <Item className='product-item flex'>
-      <div className='w-1/2'>
-        <Item.Image src={image} alt='product' />
+    <Item className="product-item flex">
+      <div className="w-1/2">
+        <Item.Image src={image} alt="product" />
       </div>
       <Item.Content>
-        <Item.Header as='a'>{name}</Item.Header>
+        <Item.Header as="a">{name}</Item.Header>
         <Item.Meta>
-          <span className='cinema'>{price} MXN</span>
+          <span className="cinema">{price} MXN</span>
         </Item.Meta>
-        <Item.Description className='mt-8'>{description}</Item.Description>
-        <Item.Extra>
+        <Item.Description className="mt-8">{description}</Item.Description>
+        <Item.Extra className='mt-8 text-center'>
           <Label>Category here</Label>
-          <Label icon='globe' content='Additional Languages' />
-          <Button.Group floated='right'>
-            <Button primary>
-              Buy tickets
-              {/* <Icon name='right chevron' /> */}
+          <div className="flex justify-center text-center mt-8">
+            <Counter
+              onIncrease={handleChangeProductQty}
+              onDecrease={handleChangeProductQty}
+              onChange={handleQtyInputChange}
+              count={quantity}
+              extraClassName={'mr-2'}
+            />
+            <Button onClick={handleAddToCart} variant="primary" disabled={isAddingToCart}>
+              {isAddingToCart ? 'Adding...' : 'Add'}
             </Button>
-          </Button.Group>
+          </div>
         </Item.Extra>
       </Item.Content>
     </Item>
   );
 };
-
