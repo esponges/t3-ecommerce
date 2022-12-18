b94986c8e96c3dc7c7cc23c1241cc337502372f5
esponges <esponges@gmail.com>
2022-12-17 18:18:57 -0600

    custom dropdown

---
+++ src/components/molecules/dropdown.tsx
@@ -0,0 +1,61 @@
+import { useOutsideClick } from "@/lib/hooks/useOutsideClick";
+import { useRef, useState } from "react";
+import { Icon } from "semantic-ui-react";
+
+type Option = { label: string; value: string, onClick?: () => void };
+
+export interface DropdownProps {
+  trigger: React.ReactNode;
+  options: Option[];
+}
+export const Dropdown = ({ options, trigger }: DropdownProps) => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
+
+  // outside click
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  useOutsideClick({
+    ref: dropdownRef,
+    onOutsideClick: () => setOpen(false),
+  });
+
+  const handleSelect = (option: Option) => {
+    setSelected(option);
+    // setSelectedOption(option);
+    setOpen(false);
+
+    if (option.onClick) {
+      option.onClick();
+    }
+  };
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative" ref={dropdownRef}>
+      <div className="flex items-center" onClick={handleOpen}>
+        {trigger}
+        <div className="ml-2">
+          <Icon name="chevron down" />
+        </div>
+      </div>
+      <div className={`absolute top-0 left-0 w-full z-10 ${open ? 'block' : 'hidden'}`}>
+        <div className="bg-white shadow rounded-md overflow-hidden">
+          {options.map((option) => (
+            <div
+              key={option.value}
+              className={`px-4 py-2 cursor-pointer hover:bg-gray-100 ${
+                selected?.value === option.value ? 'bg-gray-100' : ''
+              }`}
+              onClick={() => handleSelect(option)}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
