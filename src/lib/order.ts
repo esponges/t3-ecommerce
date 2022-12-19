9f5c0969d97a5d4b4de2ba0fdf8e27e4334e5a1a
esponges <esponges@gmail.com>
2022-12-19 13:51:09 -0600

    dont forget considering qty

--- src/lib/order.ts
+++ src/lib/order.ts
@@ -1,7 +1,9 @@
 import type { Product } from '@prisma/client';
 
-export const getOrderTotal = (products: Product[]): number => {
+type P = Product & { qty: number };
+
+export const getOrderTotal = (products: P[]): number => {
   return products.reduce((acc, product) => {
-    return acc + product.price;
+    return acc + product.price * product.qty;
   }, 0);
 };
