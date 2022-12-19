012abfc3bde74d498cd4b84409d74f30242aee8a
esponges <esponges@gmail.com>
2022-12-19 12:33:55 -0600

    add order cards

--- src/pages/auth/account/index.tsx
+++ src/pages/auth/account/index.tsx
@@ -1,26 +1,34 @@
 import { useSession } from "next-auth/react";
 
 import { trpc } from "@/utils/trpc";
+
+import type { OrderDetails } from "@/types";
 import type { User } from "@prisma/client";
+
+import { OrderCard } from "@/components/organisms/orderCard";
 
 const AccountDetails = () => {
   const { data: session } = useSession();
   const user: User | undefined = session?.user as User | undefined;
   
-  const { data } = trpc.order.getByUserId.useQuery({
+  const { data: orderData } = trpc.order.getByUserId.useQuery({
     userId: user?.id,
+    orderItemDetails: true,
   }, {
     enabled: !!user?.id,
   });
 
   return (
-    <div>
-      <h1>Account Details</h1>
-      <p>Orders</p>
+    <div className="my-10 mx-auto md:w-1/2">
+      <h1 className="mb-10">Account Details</h1>
+      <h2>Orders</h2>
       <ul>
-        {data?.map((order) => (
+        {orderData?.map((order) => (
           <li key={order.id}>
-            {order.id}
+            <OrderCard
+              order={order as OrderDetails}
+              user={user}
+            />
           </li>
         ))}
       </ul>
