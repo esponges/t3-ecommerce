8181ea8322ee5b14e537e97118f25b3cc882c81c
esponges <esponges@gmail.com>
2022-12-23 16:12:41 -0600

    update auth wrapper

--- src/components/layouts/protected.tsx
+++ src/components/layouts/protected.tsx
@@ -1,50 +1,44 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { Loader } from '../molecules/loader';
 
 type Props = {
   children: React.ReactElement;
 };
 
+/*
+  add the requireAuth property to the page component
+  to protect the page from unauthenticated users
+  e.g.:
+  OrderDetail.requireAuth = true;
+  export default OrderDetail;
+ */
+
 export const ProtectedLayout = ({ children }: Props): JSX.Element => {
   const router = useRouter();
-  const { data: session, status: sessionStatus } = useSession();
-  const [authorized, setAuthorized] = useState(false);
+  const { status: sessionStatus } = useSession();
+  const authorized = sessionStatus === 'authenticated';  
+  const unAuthorized = sessionStatus === 'unauthenticated';
+  const loading = sessionStatus === 'loading';
 
-  const authCheck = (url:string) => {
-    // redirect to login page if accessing a private page and not logged in
-    const publicPaths = ['/'];
-    const path = url.split('?')[0];
-    if (!session && sessionStatus === 'loading' && path && !publicPaths.includes(path)) {
-      setAuthorized(false);
+  useEffect(() => {
+    if (loading || !router.isReady) return;
+
+    if (unAuthorized) {
+      console.log('not authorized')
       // router.push({
       //   pathname: '/login',
       //   query: { returnUrl: router.asPath },
       // });
-    } else {
-      setAuthorized(true);
     }
-  };
+  }, [loading, unAuthorized, sessionStatus, router]);
 
-  useEffect(() => {
-    // on initial load - run auth check
-    authCheck(router.asPath);
-
-    // on route change start - hide page content by setting authorized to false
-    const hideContent = () => setAuthorized(false);
-    router.events.on('routeChangeStart', hideContent);
-
-    // on route change complete - run auth check
-    router.events.on('routeChangeComplete', authCheck);
-
-    // unsubscribe from events in useEffect return function
-    return () => {
-      router.events.off('routeChangeStart', hideContent);
-      router.events.off('routeChangeComplete', authCheck);
-    };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (loading) {
+    return (
+      <Loader />
+    )
+  }
 
   // return authorized ? <div>{children}</div> : <></>;
   return <div>{children}</div>;
