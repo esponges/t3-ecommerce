d529b10f7a0c80728d627368a9216528eba5e43d
esponges <esponges@gmail.com>
2022-12-26 16:47:42 -0600

    typing container

--- src/components/layouts/pageContainer.tsx
+++ src/components/layouts/pageContainer.tsx
@@ -1,4 +1,10 @@
-export const PageContainer = ({children}: {children: React.ReactNode}) => {
+type Props = {
+  children: React.ReactNode;
+  // todo: add some reusable page header
+  pageHeader?: React.ReactNode;
+};
+
+export const PageContainer = ({children}: Props) => {
   return (
     <div className="my-10 mx-auto md:w-1/2">
       {children}
