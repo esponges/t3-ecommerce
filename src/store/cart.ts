3a5d71e99a82f9d5c89e517029a0b806ae214b95
esponges <esponges@gmail.com>
2023-01-02 18:00:37 -0600

    refactor exports to avoid unnecessary rerenders

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -5,9 +5,11 @@ export type Item = Product & { quantity: number };
 export type CartItems = { [key: string]: Item } | Record<string, never>;
 interface CartState {
   items: CartItems;
-  restoreCart: (cart: CartItems) => void;
-  addToCart: (product: Product, quantity: number) => void;
-  removeFromCart: (productId: string) => void;
+  actions: {
+    restoreCart: (cart: CartItems) => void;
+    addToCart: (product: Product, quantity: number) => void;
+    removeFromCart: (productId: string) => void;
+  };
 }
 
 // todo: fix this any warning
@@ -15,46 +17,56 @@ interface CartState {
 const storageItems: CartItems =
   typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('cart') || '{}') : {};
 
-export const useCartStore = create<CartState>((set) => ({
+const useCartStore = create<CartState>((set) => ({
   items: storageItems,
-  // move actions to separate file
-  restoreCart: (cartItems?: CartItems) => {
-    set({ items: cartItems ?? {} });
-  },
-  addToCart: (product, quantity) => {
-    set((state) => {
-      const item: Item = { ...product, quantity };
+  actions: {
+    restoreCart: (cartItems?: CartItems) => {
+      set({ items: cartItems ?? {} });
+    },
+    addToCart: (product, quantity) => {
+      set((state) => {
+        const item: Item = { ...product, quantity };
 
-      const updatedCart = {
-        items: {
-          ...state.items,
-          [product.id]: {
-            ...item,
-            quantity: (state.items[product.id]?.quantity || 0) + quantity,
+        const updatedCart = {
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...item,
+              quantity: (state.items[product.id]?.quantity || 0) + quantity,
+            },
           },
-        },
-      };
+        };
 
-      // persist cart to local storage
-      localStorage.setItem('cart', JSON.stringify(updatedCart.items));
+        // persist cart to local storage
+        localStorage.setItem('cart', JSON.stringify(updatedCart.items));
 
-      return updatedCart;
-    });
-  },
-  removeFromCart: (productId: string) => {
-    set((state) => {
-      const updatedCart = {
-        items: {
-          ...state.items,
-        },
-      };
+        return updatedCart;
+      });
+    },
+    removeFromCart: (productId: string) => {
+      set((state) => {
+        const updatedCart = {
+          items: {
+            ...state.items,
+          },
+        };
 
-      delete updatedCart.items[productId];
+        delete updatedCart.items[productId];
 
-      // persist cart to local storage
-      localStorage.setItem('cart', JSON.stringify(updatedCart.items));
+        // persist cart to local storage
+        localStorage.setItem('cart', JSON.stringify(updatedCart.items));
 
-      return updatedCart;
-    });
-  },
+        return updatedCart;
+      });
+    }
+  }
+
 }));
+
+// export selectors and actions
+// if we subscribe directly to the store in the component
+// we will get a new object every time the store changes
+// and the component will re-render at every change in the store
+export const useCartItems = () => useCartStore((state) => state.items);
+export const useCartActions = () => useCartStore((state) => state.actions);
+
