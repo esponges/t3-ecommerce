ce2846e85f77ba4a70138a0a381b0f2499804088
esponges <esponges@gmail.com>
2023-01-17 19:05:33 -0600

    add constants to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-blue": "var(--color-primary-blue)",
+        "secondary-blue": "var(--color-secondary-blue)",
+      },
+    },
   },
   plugins: [],
 };
