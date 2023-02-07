3e6b06a97cff19ae4f5c4ebd7336397a2d640b81
esponges <esponges@gmail.com>
2023-02-07 16:25:16 -0600

    order categories desc

--- src/server/trpc/router/category.ts
+++ src/server/trpc/router/category.ts
@@ -1,8 +1,13 @@
-import { t } from "../trpc";
-
+import { z } from 'zod';
+import { t } from '../trpc';
 
 export const categoryRouter = t.router({
-  getAll: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.category.findMany();
-  }),
+  getAll: t.procedure
+    .query(({ ctx }) => {
+      return ctx.prisma.category.findMany({
+        orderBy: {
+          score: 'desc'
+        },
+      });
+    }),
 });
