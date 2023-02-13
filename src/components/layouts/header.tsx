907e25f119a101fdd11feeba15fe05cf10bab8cd
esponges <esponges@gmail.com>
2023-02-13 14:00:29 -0600

    use white header logo

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -135,7 +135,7 @@ export const Header = ({ children }: Props) => {
                   <a>
                     <Image
                       // eslint-disable-next-line max-len
-                      path="https://ik.imagekit.io/5wjtgrwr1/logo-no-slogan.png?ik-sdk-version=javascript-1.4.3&updatedAt=1674056851908"
+                      path="https://ik.imagekit.io/5wjtgrwr1/logo-no-bg-white.png?ik-sdk-version=javascript-1.4.3&updatedAt=1676318338413"
                       alt="logo-vinoreo-header"
                       width={100}
                       height={100}
@@ -178,7 +178,7 @@ export const Header = ({ children }: Props) => {
                   <div className="absolute left-[45%] hover:cursor-pointer">
                     <Image
                       // eslint-disable-next-line max-len
-                      path="https://ik.imagekit.io/5wjtgrwr1/logo-no-slogan.png?ik-sdk-version=javascript-1.4.3&updatedAt=1674056851908"
+                      path="https://ik.imagekit.io/5wjtgrwr1/logo-no-bg-white.png?ik-sdk-version=javascript-1.4.3&updatedAt=1676318338413"
                       alt="logo-vinoreo-header"
                       width={100}
                       height={100}
