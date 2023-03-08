00714d32a4eb7c9e3a0f38b6fe379e98ecbf9e1b
esponges <esponges@gmail.com>
2023-03-07 18:34:39 -0600

    add fallback for errors in iki image

--- src/components/atoms/image.tsx
+++ src/components/atoms/image.tsx
@@ -4,6 +4,7 @@ import { IKImage } from 'imagekitio-react';
 
 import type { ImageProps } from 'next/image';
 import type { IKImageProps } from 'imagekitio-react/dist/types/components/IKImage/combinedProps';
+import { useState } from 'react';
 
 type Props = IKImageProps & ImageProps & {
   src: string;
@@ -28,8 +29,9 @@ const getIsIKImage = (src?: string) => src?.match(/https:\/\/ik\.imagekit\.io\/[
 // the Next.js Image component which is more performant
 export const Image = (props: Props) => {
   const isIKImage = getIsIKImage(props.src);
+  const [hasIKIError, setHasIKIError] = useState(false);
 
-  if (!isIKImage) {
+  if (!isIKImage || hasIKIError) {
     return (
       <NextImage
         alt={props.alt || 'image'}
@@ -39,16 +41,24 @@ export const Image = (props: Props) => {
         width={props.width || 300}
         height={props.height || 300}
         {...props}
-        src={props.src}
+        src={!hasIKIError ? props.src : '/images/empty-bottle.png'}
       />
     );
   }
 
   const IKIProps = getIKIProps(props);
 
+  const handleIKIError = () => {
+    console.warn('ImageKit error', props.src);
+    setHasIKIError(true);
+  };
+
   return (
     <div className='iki-img-container'>
-      <IKImage 
+      <IKImage
+        loading='lazy'
+        onError={handleIKIError}
+        lqip={{ active: true, quality: 20 }}
         {...IKIProps}
       />
     </div>
