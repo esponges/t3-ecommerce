6bb728fa289caf40af8f1aa33b9f28860b26986a
esponges <esponges@gmail.com>
2023-03-28 17:07:09 -0600

    playwright config and localhost test PoC

--- src/__tests__/e2e/test.spec.ts
+++ src/__tests__/e2e/test.spec.ts
@@ -3,8 +3,12 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('img', { name: 'product' }).first().click();
-  await page.getByRole('heading', { name: 'pangoro' }).click();
-  await page.getByRole('button', { name: 'plus' }).first().dblclick();
-  await page.getByRole('button', { name: 'Shop now' }).click();
-  await page.getByRole('alert').filter({ hasText: 'pangoro (3x) agregado' }).click();
+  await page.getByRole('link', { name: 'Cart' }).click();
+  await page.getByText('Your cart is empty').click();
+
+  // go to cart
+
+  // expect datatest-id 'product-item-name' to be in the page
+  // const productItemName = await page.$('[datatest-id="product-item-name"]');
+  // expect(productItemName).toBeTruthy();
 });
