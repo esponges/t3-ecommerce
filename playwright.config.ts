3d6acef7f730077fdbe6e8d2771b1b6ac3277c60
esponges <esponges@gmail.com>
2023-04-03 17:13:25 -0600

    cleanup for tests after fixing error with authed context

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,21 +11,10 @@ export default defineConfig({
   // Run all tests in parallel.
   fullyParallel: true,
 
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  // forbidOnly: !!process.env.CI,
-
-  // Retry on CI only.
-  // retries: process.env.CI ? 2 : 0,
-
-  // Opt out of parallel tests on CI.
-  // workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
-  // reporter: 'html',
-
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://127.0.0.1:3000',
+    // the PLAYWRIGHT_TEST_BASE_URL env var is set in the github action
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
 
     // Collect trace when retrying the failed test.
     // trace: 'on-first-retry',
@@ -40,10 +29,4 @@ export default defineConfig({
       },
     },
   ],
-  // Run your local dev server before starting the tests.
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- src/__tests__/e2e/setup/global.ts
+++ src/__tests__/e2e/setup/global.ts
@@ -10,12 +10,15 @@ const testCookie: Cookie = {
   name: 'next-auth.session-token',
   value: 'd52f0c50-b8e3-4326-b48c-4d4a66fdeb64', // some random id
   domain: 'localhost',
+  // url: 'localhost',
   path: '/',
-  expires: 1678926378, // some random date, can be expired doesn't matter
+  expires: -1, // some random future date, can be expired doesn't matter
   httpOnly: true,
   secure: false,
   sameSite: 'Lax',
 };
+
+export const mockedUserName = 'Octocat';
 
 export default async function globalSetup() {
   const now = new Date();
@@ -25,7 +28,7 @@ export default async function globalSetup() {
       email: 'octocat@github.com',
     },
     create: {
-      name: 'Octocat',
+      name: mockedUserName,
       email: 'octocat@github.com',
       image: 'https://github.com/octocat.png',
       sessions: {
@@ -57,9 +60,13 @@ export default async function globalSetup() {
     const storageState = path.resolve(__dirname, 'storage-state.json');
     const browser = await chromium.launch();
     const context = await browser.newContext({ storageState });
+
     await context.addCookies([testCookie]);
     await context.storageState({ path: storageState });
-    console.log('context.storageState', context.cookies());
+
+    // log context cookies
+    const cook = await context.cookies();
+    console.log(cook);
     await browser.close();
   } catch (err) {
     console.error(`Error loading storage state:`);
