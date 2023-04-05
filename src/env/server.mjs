6ff197796485b015a9edcbb5de3c51aa0d1c8480
esponges <esponges@gmail.com>
2023-04-04 18:07:20 -0600

    revert server check and try another mocked url

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -8,32 +8,20 @@ import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
 
-// console.log("🔐 Validating environment variables for the server...", serverSchema.data);
-console.log("🔐 Validating environment variables for the client...", clientEnv);
-
-let env = {
-  NODE_ENV: process.env.NODE_ENV || "development",
-};
-
-// bypass checks in test environment
-if (process.env.NODE_ENV !== "test") {
-  if (!_serverEnv.success) {
-    console.error(
-      "❌ Invalid environment variables:\n",
-      ...formatErrors(_serverEnv.error.format()),
-    );
-    throw new Error("Invalid environment variables");
-  }
-
-  for (let key of Object.keys(_serverEnv.data)) {
-    if (key.startsWith("NEXT_PUBLIC_")) {
-      console.warn("❌ You are exposing a server-side env-variable:", key);
-
-      throw new Error("You are exposing a server-side env-variable");
-    }
-  }
-  env = { ..._serverEnv.data, ...clientEnv };
+if (!_serverEnv.success) {
+  console.error(
+    "❌ Invalid environment variables:\n",
+    ...formatErrors(_serverEnv.error.format()),
+  );
+  throw new Error("Invalid environment variables");
 }
 
-export { env };
+for (let key of Object.keys(_serverEnv.data)) {
+  if (key.startsWith("NEXT_PUBLIC_")) {
+    console.warn("❌ You are exposing a server-side env-variable:", key);
 
+    throw new Error("You are exposing a server-side env-variable");
+  }
+}
+
+export const env = { ..._serverEnv.data, ...clientEnv };
